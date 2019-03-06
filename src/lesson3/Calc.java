da332701b77ef6b0226c89a4c1435cb45eb6e4a4
difepi <df@mymedtech.ru>
2019-03-06 12:05:03 +0300

    ДЗ Урок 3 Java 1. Калькулятор+игра угадай число.

--- src/lesson3/Calc.java
+++ src/lesson3/Calc.java
@@ -1,4 +1,79 @@
 package lesson3;
 
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+
 public class Calc {
+    static private Scanner in = new Scanner(System.in);
+
+
+    public static void main (String[]args){
+        consoleCalc();
+    }
+
+    private static boolean chekString (String string){
+        boolean flag = true;
+        for (int i=0; i<string.length(); i++) {
+
+            if ((string.charAt(i) <= '9') && (string.charAt(i) >= '0')) {
+                flag = false;
+            }
+            else {
+                switch (string.charAt(i)) {
+                    case '-':
+                        flag = false;
+                        break;
+                    case '+':
+                        flag = false;
+                        break;
+                    case '*':
+                        flag = false;
+                        break;
+                    case '/':
+                        flag = false;
+                        break;
+                    default:
+                        return true;
+
+                }
+            }
+        }
+        return flag;
+    }
+
+    private static void consoleCalc() {
+        String[] line=  in.nextLine().split(" ");
+        for (String res : line) {
+            if (chekString(res)) {
+                System.out.printf("%s Не является числом и математическим знаком \t", res);
+            }
+        }
+
+        int result = Integer.parseInt(line[0]);
+
+        for (int i = 0; i < line.length; i++) {
+            switch (line[i]) {
+                case "+":
+                    result += Integer.parseInt(line[i + 1]);
+                    break;
+                case "-":
+                    result -= Integer.parseInt(line[i + 1]);
+                    break;
+                case "*":
+                    result *= Integer.parseInt(line[i + 1]);
+                    break;
+                case "/":
+                    result /= Integer.parseInt(line[i + 1]);
+                    break;
+            }
+        }
+
+        System.out.printf("Результат вычислений = %d", result);
+    }
+
 }
+
+
+
--- src/lesson3/Main_DZ.java
+++ src/lesson3/Main_DZ.java
@@ -1,4 +1,69 @@
 package lesson3;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main_DZ {
+    public static void main(String[] args) {
+        System.out.printf (" 1. Написать программу, которая загадывает случайное число от 0 до 9, \n \t " +
+                "и пользователю дается 3 попытки угадать это число. При каждой попытке компьютер должен сообщить \n\t" +
+                " больше ли указанное пользователем число чем загаданное, или меньше. После победы или проигрыша\n\t" +
+                " выводится запрос – «Повторить игру еще раз? 1 – да / 0 – нет»(1 – повторить, 0 – нет).\n");
+
+        guessRandom();
+    }
+
+    public static void  guessRandom (){
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+        int cls = 1;
+        int attempt = 0;
+
+        do {
+            int xchislo = random.nextInt(9);
+
+            for (int i=1; i<4;i++){
+                System.out.printf ("%d Попытка.  Введите число: ",i);
+                int chislo = sc.nextInt();
+                chislo = rangeChek(chislo,9,0);
+
+
+                if (chislo==xchislo) {
+                    System.out.println (" Вы угадали! ");
+                    break;
+                } else {
+                    if (chislo < xchislo) {
+                        System.out.printf (" Загаданное число больше введенного. У Вас осталось %d попытки \n",3-i);
+                    } else  {
+                        if (chislo > xchislo) {
+                            System.out.printf (" Загаданное число меньше введеного. У Вас осталось %d попытки \n",3-i);
+                        }
+
+                    }
+                }
+                attempt = i;
+            }
+            if  (attempt==3) {
+                System.out.printf (" Вы проиграли. Загаданное число было %d \n", xchislo);
+            }
+            System.out.println ("Повторить игру еще раз? 1 – да / 0 – нет:  ");
+            cls = sc.nextInt();
+
+        } while (cls == 1);
+
+        sc.close();
+    }
+    public static int rangeChek (int x, int max, int min){
+        int numrange=x;
+        Scanner sc = new Scanner(System.in);
+
+        while ((numrange > max)||(numrange < min)){
+            System.out.print (" Введение число не взодит в доипазон от 0 до 9.\n Повторите ввод: ");
+            numrange = sc.nextInt();
+        }
+
+        return (numrange);
+
+    }
+
 }
