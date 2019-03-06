71faf3d2096a12ad694ad7e96620811a5357ce8c
difepi <df@mymedtech.ru>
2019-03-06 23:04:33 +0300

    В калькулятор добавлен перехват исключений try catch

--- src/lesson3/Calc.java
+++ src/lesson3/Calc.java
@@ -9,18 +9,17 @@ public class Calc {
     static private Scanner in = new Scanner(System.in);
 
 
-    public static void main (String[]args){
+    public static void main(String[] args) {
         consoleCalc();
     }
 
-    private static boolean chekString (String string){
+    private static boolean chekString(String string) {
         boolean flag = true;
-        for (int i=0; i<string.length(); i++) {
+        for (int i = 0; i < string.length(); i++) {
 
             if ((string.charAt(i) <= '9') && (string.charAt(i) >= '0')) {
                 flag = false;
-            }
-            else {
+            } else {
                 switch (string.charAt(i)) {
                     case '-':
                         flag = false;
@@ -44,36 +43,43 @@ public class Calc {
     }
 
     private static void consoleCalc() {
-        String[] line=  in.nextLine().split(" ");
+
+        String[] line = in.nextLine().split(" ");
         for (String res : line) {
             if (chekString(res)) {
                 System.out.printf("%s Не является числом и математическим знаком \t", res);
             }
         }
 
-        int result = Integer.parseInt(line[0]);
 
-        for (int i = 0; i < line.length; i++) {
-            switch (line[i]) {
-                case "+":
-                    result += Integer.parseInt(line[i + 1]);
-                    break;
-                case "-":
-                    result -= Integer.parseInt(line[i + 1]);
-                    break;
-                case "*":
-                    result *= Integer.parseInt(line[i + 1]);
-                    break;
-                case "/":
-                    result /= Integer.parseInt(line[i + 1]);
-                    break;
+        try {
+            int result = Integer.parseInt(line[0]);
+
+
+            for (int i = 0; i < line.length; i++) {
+                switch (line[i]) {
+                    case "+":
+                        result += Integer.parseInt(line[i + 1]);
+                        break;
+                    case "-":
+                        result -= Integer.parseInt(line[i + 1]);
+                        break;
+                    case "*":
+                        result *= Integer.parseInt(line[i + 1]);
+                        break;
+                    case "/":
+                        result /= Integer.parseInt(line[i + 1]);
+                        break;
+                }
             }
+            System.out.printf("Результат вычислений = %d", result);
+        } catch (NumberFormatException e) {
+            System.out.println("Эта версия калькулятора не умеет считать числа. До свидания!");
         }
-
-        System.out.printf("Результат вычислений = %d", result);
     }
 
 }
+
 
 
 
